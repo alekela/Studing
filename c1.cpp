d52803e3a788ee697b7b225c5011bfd6fc112c80
alekela <76111160+alekela@users.noreply.github.com>
2023-09-14 16:49:18 +0300

    Add files via upload

--- c1.cpp
+++ c1.cpp
@@ -12,9 +12,17 @@ int main(){
     int* arr1 = new int[5];
     int* arr2 = new int[5] {1, 2, 3, 4, 5};
     int arr3[5] = {1, 2, 3, 4, 5};
+    int* arr4 = (int *) malloc(sizeof(int) * 5);
+    int * arr5 = (int *) calloc(5, sizeof(int));
+    int* arr6;
+    arr6 = (int *) realloc(arr6, 5 * sizeof(int));
 
     for (int i = 0; i < 5; i++){
-        cout << vect[i] << "\n";
+        arr6[i] = i;
+    }
+    
+    for (int i = 0; i < 5; i++){
+        cout << arr6[i] << "\n";
     }
 
     return 0;
