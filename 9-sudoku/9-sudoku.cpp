a65f4e192a61c6d562ed1440afa9ad8079c8ee11
Алексей Чурсин <76111160+alekela@users.noreply.github.com>
2023-10-27 13:45:43 +0300

    Update 9-sudoku.cpp

--- 9-sudoku/9-sudoku.cpp
+++ 9-sudoku/9-sudoku.cpp
@@ -16,33 +16,47 @@ bool check(int n, vector<int> nums) {
 }
 
 
-void find(int s, int start, vector<int> mini_answer, vector<int> numbers, int n) {
+void find(int s, int start_s, int start, vector<int> &answer, vector<int> &numbers, int n) {
     if (s == 0) {
-        if (mini_answer.size() == n) {
-            for (int i : mini_answer) {
+        int sum = 0;
+        for (int i : answer) {
+            sum += i;
+        }
+        if (answer.size() == n && sum == start_s) {
+            for (int i : answer) {
                 cout << i << " ";
             }
             cout << endl;
         }
-        mini_answer.clear();
+        answer.clear();
         return;
     }
     for (int i = start; i < numbers.size(); i++) {
         if (numbers[i] <= s) {
-            mini_answer.push_back(numbers[i]);
-            find(s - numbers[i], i + 1, mini_answer, numbers, n);
+            answer.push_back(numbers[i]);
+            find(s - numbers[i], start_s, i + 1, answer, numbers, n);
         }
     }
-    mini_answer.clear();
+    answer.clear();
 }
 
 
 int main() {
+    string tmp;
+    vector<int> banned_n;
+    cout << "Введите забаненные цифры: " << endl;
+    std::getline(std::cin, tmp);
     int s;
+    cout << "Введите сумму, которую надо получить: " << endl;
     cin >> s;
     int n;
+    cout << "Введите количество цифр: " << endl;
     cin >> n;
-    vector<int> banned_n = {3, 7};
+    for (int i = 0; i < tmp.size(); i++) {
+        if (isdigit(tmp[i])) {
+            banned_n.push_back(tmp[i] - '0');
+        }
+    }
     int numbers[9] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
     vector<int> clear_numbers;
     for (int i = 0; i < 9; i++) {
@@ -50,6 +64,6 @@ int main() {
             clear_numbers.push_back(numbers[i]);
         }
     }
-    vector<int> mini_answer;
-    find(s, 0, mini_answer, clear_numbers, n);
+    vector<int> answer;
+    find(s, s, 0, answer, clear_numbers, n);
 }
