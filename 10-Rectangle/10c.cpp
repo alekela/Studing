39b4fccb93d970f63af4343706e84a639383b5cb
Алексей Чурсин <76111160+alekela@users.noreply.github.com>
2023-10-27 17:04:25 +0300

    Add files via upload

---
+++ 10-Rectangle/10c.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Rectangle.cpp"
+
+int main() {
+    Rectangle rect1(1, 4, 3, 2), rect2(2, 3, 4, 1);
+    if (rect2.check_collision(rect1)) {
+        cout << "True";
+    }
+    else {
+        cout << "False";
+    }
+
+}
