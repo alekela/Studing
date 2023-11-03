8eec633a4d90b137fa20d2543870f6a183b08765
Алексей Чурсин <76111160+alekela@users.noreply.github.com>
2023-11-03 13:36:37 +0300

    Delete 8-OOP-pole-chudes directory

--- 8-OOP-pole-chudes/8-OOP.cpp
+++
@@ -1,9 +0,0 @@
-#include "8c_1.cpp"
-
-
-int main() {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-    Pole_chudes game;
-    game.start_game();
-}
--- 8-OOP-pole-chudes/c8.cpp
+++
@@ -1,9 +0,0 @@
-#include "Output_word.h"
-#include "Narator.h"
-#include "Pole_chudes.h"
-
-
-int main() {
-    Pole_chudes game;
-    game.start_game();
-}
